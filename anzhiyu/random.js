ad95b86c662e8ba1e8102b261201dcdfd20084c6
BackRer <3100026750@qq.com>
2023-06-27 18:13:06 +0800

    Site updated: 2023-06-27 18:12:59

---
+++ anzhiyu/random.js
@@ -0,0 +1,1 @@
+var posts=["2023/06/27/McModMade/","2023/06/27/front-matter/","2023/06/27/hello-world/","2023/06/27/text/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
