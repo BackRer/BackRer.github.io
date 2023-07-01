f12c9dacffa1ac69a08737848ac088812970bdd5
BackRer <3100026750@qq.com>
2023-07-01 20:25:03 +0800

    Site updated: 2023-07-01 20:24:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/07/01/BukkitApiCooldown/","2023/06/27/McModMade/","2023/06/27/front-matter/","2023/06/27/hello-world/","2023/07/01/BukkitTools/","2023/06/27/text/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/07/01/BukkitApiCooldown/","2023/06/27/front-matter/","2023/06/27/hello-world/","2023/07/01/BukkitTools/","2023/06/27/text/","2023/06/27/McModMade/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
